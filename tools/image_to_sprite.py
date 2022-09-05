361dad062010acf546090c687a2a3831e00848d1
Ice Lam <pinky.yy.lam@gmail.com>
2022-09-05 13:36:29 +0800

    fix(tools): fix transparent pixel issue when converting image to sprite

--- tools/image_to_sprite.py
+++ tools/image_to_sprite.py
@@ -24,19 +24,21 @@ class InvalidArgumentsError(Exception):
         message = f'Invalid number of arguments. Usage: python {sys.argv[0]} <filename>'
         super().__init__(f'\033[91m{message}\033[0m')
 
+class UnsupportedFileFormatError(Exception):
+    """Raised when image path provided as argument does not exists"""
+    def __init__(self, file_extension):
+        message = f'{file_extension} is not supported. Only PNG and JPEG images are supported'
+        super().__init__(f'\033[91m{message}\033[0m')
+
 def get_image_path():
     """Extract image path from arguments"""
-    try:
-        if len(sys.argv) != 2:
-            raise InvalidArgumentsError()
+    if len(sys.argv) != 2:
+        raise InvalidArgumentsError()
 
-        if not os.path.exists(sys.argv[1]):
-            raise FileNotExistError(sys.argv[1])
+    if not os.path.exists(sys.argv[1]):
+        raise FileNotExistError(sys.argv[1])
 
-        return sys.argv[1]
-    except Exception as error: # pylint: disable=broad-except
-        print(error)
-        sys.exit(0)
+    return sys.argv[1]
 
 def resize_image(file_path, size):
     """
@@ -44,9 +46,18 @@ def resize_image(file_path, size):
     As sprites are always square, image not having 1:1 ratio will be put to center of new image.
     """
     pillow_image = Image.open(file_path, 'r')
-    pillow_image.thumbnail((size, size))
 
-    background_color = (0, 0, 0)
+    image_format = pillow_image.format
+
+    if not image_format == 'JPEG' and not image_format == 'PNG':
+        raise UnsupportedFileFormatError(image_format)
+
+    original_width, original_height = pillow_image.size
+
+    if not original_width == size or not original_height == size:
+        pillow_image.thumbnail((size, size))
+
+    background_color = (0, 0, 0, 0)
     width, height = pillow_image.size
 
     # original ratio is 1:1, skip processing
@@ -55,7 +66,7 @@ def resize_image(file_path, size):
 
     # landscape image
     if width > height:
-        square_pillow_image = Image.new(pillow_image.mode, (width, width), background_color)
+        square_pillow_image = Image.new('RGBA', (width, width), background_color)
         square_pillow_image.paste(pillow_image, (0, (width - height) // 2))
         return square_pillow_image
 
@@ -67,9 +78,20 @@ def resize_image(file_path, size):
 def rgb_to_hex(rgb):
     """Convert rgb tuple to hex string"""
 
-    # remove alpha channel from PNG image
-    if len(rgb) > 3:
-        rgb = rgb[:(3 - len(rgb))]
+    # Remove alpha channel from PNG image, for transparent pixel, fill black.
+    # For black color, since cyberpi sprites will treat them as transparent,
+    # replace them with dark grey rgb(34, 34, 34)
+    if len(rgb) > 3: # PNG
+        red, green, blue, alpha = rgb
+
+        if alpha == 0:
+            rgb = (0, 0, 0)
+        elif red == 0 and green == 0 and blue == 0:
+            rgb = (34, 34, 34)
+        else:
+            rgb = rgb[:(3 - len(rgb))]
+    elif len(rgb) == 3 and rgb[0] == 0 and rgb[1] == 0 and rgb[2] == 0: # JPG
+        rgb = (34, 34, 34)
 
     return '0x%02x%02x%02x' % rgb # pylint: disable=consider-using-f-string
 
@@ -87,21 +109,31 @@ def save_to_file(output_directory, filename, data):
         os.makedirs(output_directory)
 
     result_string = f'[{", ".join(map(str, data))}]'
-    with open(output_path, "w", encoding="utf-8") as output_file:
+    with open(output_path, 'w', encoding='utf-8') as output_file:
         output_file.write(result_string)
 
     print(f'\033[92mSprite matrix have been save to {output_path}\033[0m')
 
 if __name__ == '__main__':
-    image_path = get_image_path()
-    image = resize_image(image_path, 16)
-    rgb_list = convert_to_color_matrix(image)
+    try:
+        image_path = get_image_path()
+        image = resize_image(image_path, 16)
+        rgb_list = convert_to_color_matrix(image)
 
-    program_directory = os.path.dirname(os.path.realpath(__file__))
-    parent_directory = os.path.abspath(os.path.join(program_directory, os.pardir))
+        program_directory = os.path.dirname(os.path.realpath(__file__))
+        parent_directory = os.path.abspath(os.path.join(program_directory, os.pardir))
 
-    save_to_file(
-        os.path.join(parent_directory, 'sprite-matrix'),
-        os.path.basename(image_path),
-        rgb_list
-    )
+        save_to_file(
+            os.path.join(parent_directory, 'sprite-matrix'),
+            os.path.basename(image_path),
+            rgb_list
+        )
+    except FileNotExistError as error:
+        print(error)
+        sys.exit(0)
+    except InvalidArgumentsError as error:
+        print(error)
+        sys.exit(0)
+    except UnsupportedFileFormatError as error:
+        print(error)
+        sys.exit(0)
